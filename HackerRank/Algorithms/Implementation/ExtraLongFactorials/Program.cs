3e7aa4358872f1feedc9f9b962ca7383d26ba7d8
Christina Porter <christinaleeporter@gmail.com>
2023-12-30 21:55:10 -0600

    HackerRank Extra Long Factorials

---
+++ HackerRank/Algorithms/Implementation/ExtraLongFactorials/Program.cs
@@ -0,0 +1,45 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+using System.Numerics;
+
+class Result
+{
+
+    /*
+     * Complete the 'extraLongFactorials' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void extraLongFactorials(int n)
+    {
+       //int n = 25;
+        BigInteger result = new BigInteger(n);
+        for (int i = n; i > 1; i--) //i=25
+        {
+            result = result * (i - 1); //600
+        }
+        Console.WriteLine(result);
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.extraLongFactorials(n);
+    }
+}
